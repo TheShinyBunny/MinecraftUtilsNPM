9bf4be5316f20acdd3da1c4bdb7ffa1992f420d6
TheShinyBunny <shinybunny123@gmail.com>
2020-03-29 13:45:31 +0300

    Remove out directory from repo

--- out/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { launchClient, ClientOptions, MinecraftClient } from "./client";
-import { launchServer, ServerOptions, MinecraftServer } from "./server";
-export declare function downloadUrl(url: string, dest: string): Promise<void>;
-export { launchClient, ClientOptions, MinecraftClient, launchServer, ServerOptions, MinecraftServer };
